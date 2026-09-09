10cd4c58a782d03e8f8e4e4468900866e72bf7c9
oscar-3-3 <oscar.contreras@teamsparq.com>
2026-09-09 13:15:35 -0600

    feat: update Home component to dynamically set asset path for GitHub Actions

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const assetPath = process.env.GITHUB_ACTIONS === "true" ? "/next-demo" : "";
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
+          src={`${assetPath}/next.svg`}
           alt="Next.js logo"
           width={100}
           height={20}
